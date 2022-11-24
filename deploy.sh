1bd6e85f14b7459149604b3345628d6f37d54546
Shahab Dogar <shahab@dogar.dev>
2022-11-24 12:15:32 -0800

    Remove sudo

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-aws ecr get-login-password --region us-west-2 | sudo docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
-sudo docker build --rm -t nestjs .
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
+docker build --rm -t nestjs .
 DOCKER_IMAGE=$(sudo docker images -q nestjs:latest)
-sudo docker tag $DOCKER_IMAGE $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/fargate-api-test:latest
-sudo docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/fargate-api-test:latest
+docker tag $DOCKER_IMAGE $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/fargate-api-test:latest
+docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/fargate-api-test:latest
