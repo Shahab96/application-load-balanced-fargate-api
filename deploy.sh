f52a810693004b8356db356d6b5bff11b3b2a826
Shahab Dogar <shahab@dogar.dev>
2022-11-24 12:18:43 -0800

    Make ecr repo push even more dynamic

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ACCOUNT_ID=$2
 REGION=$3
 
 aws ecr get-login-password --region $3 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$3.amazonaws.com
-docker build --rm -t nestjs .
-DOCKER_IMAGE=$(sudo docker images -q nestjs:latest)
+docker build --rm -t $1 .
+DOCKER_IMAGE=$(sudo docker images -q $1:latest)
 docker tag $DOCKER_IMAGE $ACCOUNT_ID.dkr.ecr.$3.amazonaws.com/$1:latest
 docker push $ACCOUNT_ID.dkr.ecr.$3.amazonaws.com/$1:latest
