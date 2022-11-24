725d026ea54bc9011cf461017d1baea3dd256b26
Shahab Dogar <shahab@dogar.dev>
2022-11-24 12:19:35 -0800

    Further remove sudo

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ REGION=$3
 
 aws ecr get-login-password --region $3 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$3.amazonaws.com
 docker build --rm -t $1 .
-DOCKER_IMAGE=$(sudo docker images -q $1:latest)
+DOCKER_IMAGE=$(docker images -q $1:latest)
 docker tag $DOCKER_IMAGE $ACCOUNT_ID.dkr.ecr.$3.amazonaws.com/$1:latest
 docker push $ACCOUNT_ID.dkr.ecr.$3.amazonaws.com/$1:latest
